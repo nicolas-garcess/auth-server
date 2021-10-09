8e390a2b99295f4ede7f6dda65b55c7fab4250f4
nicolas-garcess <nicolasgarces95@gmail.com>
2021-10-09 18:28:24 -0500

    schemas created

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const authRoutes = require('./routes');
 require('dotenv').config();
 
 const app = express();
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 // Settings
 app.set('port', process.env.PORT || 3500);
@@ -10,5 +14,7 @@ app.set('port', process.env.PORT || 3500);
 app.get('/', (req, res) => {
   res.send('Hola, bienvenido al servidor');
 });
+
+app.use('/api/user', authRoutes);
 
 module.exports = app;
